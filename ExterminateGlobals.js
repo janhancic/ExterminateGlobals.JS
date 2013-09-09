f52fc640f34dd5757978b89625ffd84900f04879
Jan Hančič <jan.hancic@gmail.com>
2013-09-09 21:12:06 +0100

    Add code to detect globals

--- ExterminateGlobals.js
+++ ExterminateGlobals.js
@@ -1,26 +1,74 @@
 window.ExterminateGlobals = (function () {
 	var app = {},
 		_monitorObject,
-		_ignoreKeys;
+		_ignoreKeys,
+		_startGlobals;
 
 	/**
 	 * Start monitoring the `monitorObject` for unwanted globals.
 	 * 
-	 * @param {Object} monitorObject (optional) The object to monitor, 
-	 *  defaults to `window`
 	 * @param {Array} ignoreKeys (optional) A list of keys to ignore,
 	 *  you would put your known globals here.
+	 * @param {Object} monitorObject (optional) The object to monitor, 
+	 *  defaults to `window`
 	 */
-	app.start = function ( monitorObject, ignoreKeys ) {
+	app.start = function ( ignoreKeys, monitorObject ) {
+		_ignoreKeys = ignoreKeys || [ 'ExterminateGlobals' ];
 		_monitorObject = monitorObject || window;
-		_ignoreKeys = ignoreKeys || [];
+
+		if ( _ignoreKeys.indexOf( 'ExterminateGlobals' ) === -1 ) {
+			_ignoreKeys.push( 'ExterminateGlobals' );
+		}
+
+		_startGlobals = _collectMembers();
 	};
 
 	/**
 	 * Stops the monitoring and reports (to console) any found globals.
 	 */
 	app.stop = function () {
+		var endGlobals = _collectMembers(),
+			numOfUnknownGlobals = endGlobals.length - _startGlobals.length,
+			unknownGlobals,
+			tableData = [];
 
+		console.group( '==========> ExterminateGlobals.JS <==========' );
+
+		if ( numOfUnknownGlobals === 0 ) {
+			console.info( 'No unknown globals found! Nothing to exterminate :(' );
+			console.groupEnd();
+			return;
+		}
+
+		console.info( 'Found ' + numOfUnknownGlobals + ' unknown globals! Exterminate! Exterminate! Exterminate!' );
+
+		unknownGlobals = endGlobals.filter( function ( key ) {
+			if ( _startGlobals.indexOf( key ) === -1 )  {
+				return true; // a unknown global found!
+			} else {
+				return false;
+			}
+		} );
+
+		console.info( '' );
+		console.info( 'List of unknown globals: ' + unknownGlobals.join( ', ' ) );
+
+		console.groupEnd();
+	};
+
+	/** @private */
+	function _collectMembers () {
+		var memberKeys = Object.keys( _monitorObject );
+
+		memberKeys = memberKeys.filter( function ( key ) {
+			if (_ignoreKeys.indexOf( key ) === -1 ) {
+				return true;
+			} else {
+				return false; // ignore this member as it's a know global
+			}
+		} );
+
+		return memberKeys;
 	};
 
 	return app;
